437bbe457b2b61ec959a2d8a8d9c04886ac0932a
Dub26 <153666042+Dub26@users.noreply.github.com>
2025-12-14 20:35:38 +0300

    Schedule methods updating
    3 methods are done, and I add an empty constructor.

--- src/main/java/com/app/model/Schedule.java
+++ src/main/java/com/app/model/Schedule.java
@@ -7,6 +7,11 @@ public class Schedule {
     private Map<ClassRoom,ExamSession[][]> timetables;
     private int days;
     private int slotsPerDay;
+    public  Schedule(){
+        this.timetables=null;
+        this.days=0;
+        this.slotsPerDay=0;
+    }
 
     public Schedule(Map<ClassRoom,ExamSession[][]> timetables, int days, int slotsPerDay){
         this.timetables=timetables;
@@ -19,14 +24,58 @@ public class Schedule {
     public void removeSession(ExamSession session){}
 
     public boolean isRoomAvailable(ClassRoom room, int day, int startSlot, int duration){
+       //Checking is there something funny, maybe using exceptions later
+        if (day < 0 || day >= this.days || startSlot < 0 || startSlot + duration > this.slotsPerDay) {
+            return false;
+        }
+
+        ExamSession[][] roomTimetable = timetables.get(room);
+        if (roomTimetable == null) {
+            System.out.println("timetable is null");
+            return false;
+        }
+
+        for (int slot = startSlot; slot < startSlot + duration; slot++) {
+
+            if (roomTimetable[day][slot] != null) {
+                return false;
+            }
+        }
+
         return true;
     }
 
     public Schedule deepCopy(){
-        return null;
+        Schedule newSchedule=new Schedule();
+        Map<ClassRoom,ExamSession[][]> t=this.timetables; // It can be problem because of timetables is null
+        newSchedule.timetables=t;
+        newSchedule.days=this.days;
+        newSchedule.slotsPerDay=this.slotsPerDay;
+        return newSchedule;
     }
 
     public ExamSession[][] getRoomTimetables(ClassRoom room) {
-        return null;
+        ExamSession[][] originalTimetable = timetables.get(room);
+
+
+        if (originalTimetable == null) {
+            return null;
+        }
+
+        //encapsulation  for safety(I think its necessary)
+        ExamSession[][] copiedTimetable = new ExamSession[originalTimetable.length][];
+
+
+        for (int i = 0; i < originalTimetable.length; i++) {
+
+            int rowLength = originalTimetable[i].length;
+            copiedTimetable[i] = new ExamSession[rowLength];
+            for (int j = 0; j < rowLength; j++) {
+                copiedTimetable[i][j] = originalTimetable[i][j];
+            }
+        }
+
+
+        return copiedTimetable;
     }
 }
