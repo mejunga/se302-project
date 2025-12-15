e599b10f38f56a7aab0c5286aacab184690a1e60
ataGitAccount <atakemal07@hotmail.com>
2025-12-15 20:31:41 +0300

    Repository data field
    Data field of the MasterDataRepository and ScheduleRepository classes are filled.

--- src/main/java/com/app/repository/MasterDataRepository.java
+++ src/main/java/com/app/repository/MasterDataRepository.java
@@ -1,4 +1,27 @@
 package com.app.repository;
 
+import com.app.model.ClassRoom;
+import com.app.model.Course;
+import com.app.model.ExamSession;
+import com.app.model.Student;
+
+import java.util.List;
+
 public class MasterDataRepository {
+
+    private List<Student> allStudents;
+    private List<Course> allCourses;
+    private List<ClassRoom> allClassRooms;
+    private List<ExamSession> pendingSessions;
+    private String CSVFilesRootPath;
+
+    public MasterDataRepository(List<Student> allStudents, List<Course> allCourses,
+                                List<ClassRoom> allClassRooms,
+                                List<ExamSession> pendingSessions, String CSVFilesRootPath) {
+        this.allStudents = allStudents;
+        this.allCourses = allCourses;
+        this.allClassRooms = allClassRooms;
+        this.pendingSessions = pendingSessions;
+        this.CSVFilesRootPath = CSVFilesRootPath;
+    }
 }
--- src/main/java/com/app/repository/ScheduleRepository.java
+++ src/main/java/com/app/repository/ScheduleRepository.java
@@ -1,4 +1,15 @@
 package com.app.repository;
 
+import com.app.model.Schedule;
+import java.util.List;
+
 public class ScheduleRepository {
+
+    private List<Schedule> possibleSchedules;
+    private String jsonFilePath;
+
+    public ScheduleRepository(List<Schedule> possibleSchedules, String jsonFilePath) {
+        this.possibleSchedules = possibleSchedules;
+        this.jsonFilePath = jsonFilePath;
+    }
 }
